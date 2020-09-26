d9147c23c341038d6c738d1dde74f76a087c981b
shyamkumar yadav <shyamkumaryadav2003@gmail.com>
2020-09-26 12:48:50 +0530

    bug fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var serveStatic = require('serve-static')
 const path = require("path");
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 
 app.use(express.static(path.join(__dirname, 'static')));
