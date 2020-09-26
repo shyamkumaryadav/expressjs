02a57ae3cb9bbaf1d812b143b201315098839ded
shyamkumar yadav <shyamkumaryadav2003@gmail.com>
2020-09-26 12:39:25 +0530

    add static folder

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const { response, request } = require("express");
 const express = require("express");
+var serveStatic = require('serve-static')
+const path = require("path");
 
 const app = express();
+const PORT = 8000;
 
-app.get("/", (request, response) => {
-  response.send("<h1>Hello ji</h1>");
-});
 
-app.listen(8000, () => console.log("Working on http://127.0.0.1:8000"));
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.post('/', (req, res) => {
+  res.send({
+    data: [
+      { name: 'Shyamkumar', age: 15, class: ['a', 'b', 'c', 'd'] },
+      { name: 'Rajkumar', age: 25, class: ['e', 'f', 'g', 'h'] }
+    ]
+  })
+})
+app.listen(PORT, () => console.log(`🚀🚀🚀 on http://127.0.0.1:${PORT}`));
